40e9e65a6192e3859cbc1e7ca1e1cd261ac41320
wabster341 <jakub.dmitruk1999@gmail.com>
2024-10-22 13:28:59 +0200

    wprowadzilem zmiany i skorzystalem z GUI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Witaj na mojej testowej stronie React! Hura Hura ZObaczmy czy wprowadzenie zmian zadziala</h1>
+      <h1>Witaj na mojej testowej stronie React! Hura Hura ZObaczmy czy wprowadzenie zmian zadziala,
+        teraz testuje z poziomu VS code, 123
+      </h1>
       <p>To jest moja pierwsza aplikacja React uruchomiona w VS Code.</p>
     </div>
   );
