927b24eaf2862a053542c007292e40fb13994934
wabster341 <jakub.dmitruk1999@gmail.com>
2024-10-22 13:18:12 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Witaj na mojej testowej stronie React! Hura Hura</h1>
+      <p>To jest moja pierwsza aplikacja React uruchomiona w VS Code.</p>
+    </div>
+  );
+}
+
+export default App;
