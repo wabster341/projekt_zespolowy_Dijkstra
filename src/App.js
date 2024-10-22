26e064c6e7c766e8e913945aa51282a80eae6937
wabster341 <jakub.dmitruk1999@gmail.com>
2024-10-22 14:53:20 +0200

    sprawdzam czy zadziala github action

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Witaj na mojej testowej stronie React! Hura Hura ZObaczmy czy wprowadzenie zmian zadziala,
-        teraz testuje z poziomu VS code, 123
+      <h1>Witaj na mojej testowej stronie! 
+        Sprawdzam, czy zadziała mechanizmy CI/CD z github action.
       </h1>
       <p>To jest moja pierwsza aplikacja React uruchomiona w VS Code.</p>
     </div>
