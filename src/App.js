ff2476925a7eb2e73665d6ebc171e8b06ac03f97
wabster341 <jakub.dmitruk1999@gmail.com>
2024-10-22 13:22:04 +0200

    wprowadzilem zmiany po raz drugi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Witaj na mojej testowej stronie React! Hura Hura</h1>
+      <h1>Witaj na mojej testowej stronie React! Hura Hura ZObaczmy czy wprowadzenie zmian zadziala</h1>
       <p>To jest moja pierwsza aplikacja React uruchomiona w VS Code.</p>
     </div>
   );
